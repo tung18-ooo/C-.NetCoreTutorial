21a13fe2697a6298617507ff933dff62082bb78a
tung18-ooo <tungqwe1802@gmail.com>
2024-09-05 16:40:21 +0700

    C# Networking

---
+++ CS035_Networking2/Program.cs
@@ -0,0 +1,109 @@
+﻿
+using System.Net;
+using System.Text;
+
+//var url = "https://postman-echo.com/post";
+var url = "https://www.google.com/";
+//var url = "https://www.facebook.com/";
+
+var cokies = new CookieContainer();
+
+//tao chuoi handler
+var bottomHandler = new MyHttpClientHandler(cokies);
+var changeUriHandler = new ChangeUri(bottomHandler);
+var denyAccessFacebook = new DenyAccessFacebook(changeUriHandler);
+
+
+
+/*using var handler = new SocketsHttpHandler();
+
+handler.AllowAutoRedirect = true;
+handler.AutomaticDecompression = DecompressionMethods.Deflate | DecompressionMethods.GZip;
+handler.UseCookies = true;
+handler.CookieContainer = cookies;*/
+using var handler = new SocketsHttpHandler
+{
+    AllowAutoRedirect = true,
+    AutomaticDecompression = DecompressionMethods.Deflate | DecompressionMethods.GZip,
+    UseCookies = true,
+    CookieContainer = cokies,
+};
+
+//using var httpClient = new HttpClient(handler);
+
+
+using var httpClient = new HttpClient(denyAccessFacebook);
+
+using var httpRequestMessage = new HttpRequestMessage();
+
+httpRequestMessage.Method = HttpMethod.Get;
+httpRequestMessage.RequestUri = new Uri(url);
+httpRequestMessage.Headers.Add("User-Agent", "Mozilla/5.0");
+
+var para = new List<KeyValuePair<string, string>>();
+para.Add(new KeyValuePair<string, string>("key1", "value1"));
+para.Add(new KeyValuePair<string, string>("key2", "value2"));
+
+httpRequestMessage.Content = new FormUrlEncodedContent(para);
+
+var response = await httpClient.SendAsync(httpRequestMessage);
+
+cokies.GetCookies(new Uri(url)).ToList().ForEach(c =>
+{
+    Console.WriteLine($"{c.Name} {c.Value}");
+});
+
+var html = await response.Content.ReadAsStringAsync();
+Console.WriteLine(html);
+
+
+class MyHttpClientHandler : HttpClientHandler
+{
+    public MyHttpClientHandler(CookieContainer cookie_container)
+    {
+        CookieContainer = cookie_container;
+        AllowAutoRedirect = false;
+        AutomaticDecompression = DecompressionMethods.Deflate | DecompressionMethods.GZip;
+        UseCookies = true;
+    }
+    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        Console.WriteLine("Bat dau ket noi " + request.RequestUri.ToString());
+        //thuc hien truy van den Server
+        var response = await base.SendAsync(request, cancellationToken);
+        Console.WriteLine("Hoan thanh tai du lieu");
+        return response;
+    }
+}
+
+class ChangeUri : DelegatingHandler
+{
+    public ChangeUri(HttpMessageHandler innerHandler) : base(innerHandler) { }
+    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        var host = request.RequestUri.Host.ToLower();
+        Console.WriteLine($"Check in ChangeUri - {host}");
+        if (host.Contains("google.com"))
+        {
+            request.RequestUri = new Uri("https://github.com/");
+        }
+        return await base.SendAsync(request, cancellationToken);
+    }
+}
+
+class DenyAccessFacebook: DelegatingHandler
+{
+    public DenyAccessFacebook(HttpMessageHandler innerHandler) : base( innerHandler) { }
+    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        var host = request.RequestUri.Host.ToLower();
+        Console.WriteLine($"Check in DenyFacebook - {host}");
+        if (host.Contains("facebook.com"))
+        {
+            var response = new HttpResponseMessage(HttpStatusCode.OK);
+            response.Content = new ByteArrayContent(Encoding.UTF8.GetBytes("Khong duoc truy cap"));
+            return await Task.FromResult<HttpResponseMessage>(response);
+        }
+        return await base.SendAsync(request, cancellationToken);
+    }
+}
---
+++ CS036_Networking3/Program.cs
@@ -0,0 +1,134 @@
+﻿using Newtonsoft.Json;
+using System.Net;
+using System.Text;
+
+/*
+//kiem tra he thong ho tro HttpListener
+if (HttpListener.IsSupported)
+{
+    Console.WriteLine("Support HttpListener");
+}
+else
+{
+    Console.WriteLine("Not Support HttpListener");
+    throw new Exception("Not Support HttpListener");
+}
+
+var server = new HttpListener();
+
+server.Prefixes.Add("http://*:8080/");
+
+server.Start();
+Console.WriteLine("Server Http Start");
+
+do
+{
+    var context = await server.GetContextAsync();
+    Console.WriteLine("Client connected");
+    var response = context.Response;
+    var outputStream = response.OutputStream;
+
+    response.Headers.Add("content-type", "text/html");
+
+    var html = "<h1>Heloooooooooooo</h1>";
+    var bytes = Encoding.UTF8.GetBytes(html);
+    await outputStream.WriteAsync(bytes, 0, bytes.Length);
+    outputStream.Close();
+} 
+while (server.IsListening);
+
+*/
+
+
+var server = new MyHttpServer(new string[] { "http://*:8080/" });
+await server.Start();
+
+class MyHttpServer
+{
+    private HttpListener listener;
+    public MyHttpServer(string[] prifixes)
+    {
+        if (!HttpListener.IsSupported)
+        {
+            throw new Exception("HttpListener is not support");
+        }
+        listener = new HttpListener();
+        foreach (string prifix in prifixes) listener.Prefixes.Add(prifix);
+
+    }
+
+    public async Task Start()
+    {
+        listener.Start();
+        Console.WriteLine("Http Server Start");
+        do
+        {
+            Console.WriteLine(DateTime.Now.ToLongTimeString() + " Waiting a client connect");
+            var context = await listener.GetContextAsync();
+
+            await ProcessRequest(context);
+
+            Console.WriteLine(DateTime.Now.ToLongTimeString() + " client connected");
+
+        } while (listener.IsListening);
+    }
+
+    async Task ProcessRequest(HttpListenerContext context)
+    {
+        HttpListenerRequest request = context.Request;
+        HttpListenerResponse response = context.Response;
+
+        Console.WriteLine($"{request.HttpMethod} {request.RawUrl} {request.Url.AbsolutePath}");
+
+        var outputStream = response.OutputStream;
+
+        switch (request.Url.AbsolutePath)
+        {
+            case "/":
+                {
+                    var buffer = Encoding.UTF8.GetBytes("Xin chaooooooo");
+                    response.ContentLength64 = buffer.Length;
+                    await outputStream.WriteAsync(buffer, 0, buffer.Length);
+                }
+                break;
+            case "/json":
+                {
+                    response.Headers.Add("Content-Type", "application/json");
+
+                    var product = new
+                    {
+                        Name = "Mac",
+                        Price = 20000
+                    };
+                    var json = JsonConvert.SerializeObject(product);
+                    var buffer = Encoding.UTF8.GetBytes(json);
+                    response.ContentLength64 = buffer.Length;
+                    await outputStream.WriteAsync(buffer, 0, buffer.Length);
+                }
+
+                break;
+            case "/anh2.png":
+                {
+                    response.Headers.Add("Content-Type", "image/png");
+
+                    var buffer = await File.ReadAllBytesAsync("2.png");
+                    response.ContentLength64 = buffer.Length;
+                    await outputStream.WriteAsync(buffer, 0, buffer.Length);
+                }
+
+                break;
+            default:
+                {
+                    response.StatusCode = (int)HttpStatusCode.NotFound;
+
+                    var buffer = Encoding.UTF8.GetBytes("404 NOT FOUND");
+                    response.ContentLength64 = buffer.Length;
+                    await outputStream.WriteAsync(buffer, 0, buffer.Length);
+                }
+
+                break;
+        }
+
+        outputStream.Close();
+    }
+}
